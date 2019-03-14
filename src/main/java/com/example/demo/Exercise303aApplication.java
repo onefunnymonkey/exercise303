dbd859ac92d9a4d096bacedb05c5642366faf95e
GBTC408014UR <GBTC408014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-14 14:41:27 -0400

    todo w/edit capabilities

---
+++ src/main/java/com/example/demo/Exercise303aApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise303aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise303aApplication.class, args);
+	}
+
+}
